26d650ace358f840370e9a412cb74c0392b57fe9
wesam49 <wesam.anything@gmail.com>
2026-07-29 13:26:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wesam-planner-v15-1';
+const CACHE='wesam-planner-v15-2';
 const ASSETS=['./','index.html','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
