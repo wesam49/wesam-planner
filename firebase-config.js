1af030116326b69c2a6c796d6b656455d03d2b3e
wesam49 <wesam.anything@gmail.com>
2026-09-02 12:01:24 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase-Konfiguration für Wesam Planner 16.1.
+// Diese Werte sind für Web-Apps öffentlich; der Datenzugriff wird durch Authentication und Firestore Rules geschützt.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDTGM9wNjL-jvOcevrerLp0aNriCfDFJIM",
+  authDomain: "wesam-planner.firebaseapp.com",
+  projectId: "wesam-planner",
+  storageBucket: "wesam-planner.firebasestorage.app",
+  messagingSenderId: "317894742894",
+  appId: "1:317894742894:web:2cd82d3d5ad984442e677f",
+  measurementId: "G-F0FT98KLZR"
+};
